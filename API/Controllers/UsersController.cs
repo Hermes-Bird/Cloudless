28f23dd1a90c60f742fd965dd1c6fdd91304d131
Hermes-Bird <onepunch18@yandex.by>
2021-03-14 19:20:28 +0300

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController: Controller
+    {
+        private DatabaseContext dbContext { get; set; }
+        
+        public UsersController(DatabaseContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await dbContext.Users.ToListAsync();
+        } 
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+           return await dbContext.Users.FindAsync(id);
+        } 
+    }
+}
---
+++ Persistence/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+    }
+}
